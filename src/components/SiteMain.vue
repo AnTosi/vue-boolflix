43c18ea98cc8aa5ff7772d8d624926dfdd551b2c
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-19 12:20:04 +0100

    do call api chain with axios

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -36,25 +36,41 @@ export default {
         searchText: "",
         movies_url: 'https://api.themoviedb.org/3/search/movie',
         api_key: '84b0b6316c205b8b763bc2ee40ce3b0d',
+        tv_url: 'https://api.themoviedb.org/3/search/tv'
         // searchedText: "",
         }
     },
 
     methods: {
         callApi(searchText) {
-        let movieQuery = `${this.movies_url}?api_key=${this.api_key}&language=en-US&query=${searchText}&page=1&include_adult=true`;
-        // console.log(movieQuery);
-            axios
-            .get(movieQuery)
-            // .get(`https://api.themoviedb.org/3/search/movie?api_key=84b0b6316c205b8b763bc2ee40ce3b0d&language=en-US&query=dog&page=1&include_adult=false`)
+            let movieQuery = `${this.movies_url}?api_key=${this.api_key}&language=en-US&query=${searchText}&page=1&include_adult=true`;
+            let tvQuery = `${this.tv_url}?api_key=${this.api_key}&language=en-US&query=${searchText}&page=1&include_adult=true`;
+            const axiosMovieQuery = axios.get(movieQuery);
+            const axiosTvQuery = axios.get(tvQuery);
+            axiosMovieQuery
             .then((response) => {
-                // console.log(searchedText);
+
                 this.movies = response.data.results;
-                console.log(this.movies);
-                console.log(`https://api.themoviedb.org/3/search/movie?api_key=84b0b6316c205b8b763bc2ee40ce3b0d&language=it-IT&query=` + searchText + `&page=1&include_adult=true`);
-                // console.log(this.searchText);
-                // console.log(this.api_url);
-            })
+            // console.log(movieQuery);
+                
+                // .get(movieQuery)
+                // .get(`https://api.themoviedb.org/3/search/movie?api_key=84b0b6316c205b8b763bc2ee40ce3b0d&language=en-US&query=dog&page=1&include_adult=false`)
+                // .then((response) => {
+                    // console.log(searchedText);
+                    // this.movies = response.data.results;
+                    // console.log(this.movies);
+                    // console.log(`https://api.themoviedb.org/3/search/movie?api_key=84b0b6316c205b8b763bc2ee40ce3b0d&language=it-IT&query=` + searchText + `&page=1&include_adult=true`);
+                    // console.log(this.searchText);
+                    // console.log(this.api_url);
+            
+                });
+            axiosTvQuery
+            .then((response) => {
+
+                this.tv_url = response.data.results;
+                console.log(this.tv_url);
+                }
+            )
         }
     }
 }
